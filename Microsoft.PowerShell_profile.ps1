dd1b77b84fee330e03460b58998d403c5b70da11
originates <105183376+originates@users.noreply.github.com>
2022-08-18 00:58:26 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,7 +2,8 @@ Set-Location C:\home
 
 
 # used to make the PS terminal similar to my zsh terminal
+# pipe $workDir into sed 's/\(.*\)/\L\1/' to force lowercase
 Function Prompt {
-$workDir = pwd | grep ":" | sed 's/"C:"/"~"/g'
+$workDir = pwd | grep ":" | sed 's/"C:"/"~"/g' 
 "[user@laptop " + $workDir + "]$ "
 }
