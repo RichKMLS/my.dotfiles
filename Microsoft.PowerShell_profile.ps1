5c53b7cb5e7bb00223f0d43ad3f215bed1379561
originates <105183376+originates@users.noreply.github.com>
2022-08-18 00:32:27 +0000

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
 Set-Location C:\home
 
+
+# used to make the PS terminal similar to my zsh terminal
 Function Prompt {
 $workDir = pwd | grep ":" | sed 's/"C:"/"~"/g'
 "[user@laptop " + $workDir + "]$ "
